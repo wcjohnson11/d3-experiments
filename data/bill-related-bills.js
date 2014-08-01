e35265cf5643e7e96465fb3a3fae294855277a37
Nick Bottomley <nhbottomley@gmail.com>
2014-08-01 15:02:59 -0700

    add data files, update boilerplate

---
+++ data/bill-related-bills.js
@@ -0,0 +1,23 @@
+var billRelatedBills = {
+   "status":"OK",
+   "copyright":"Copyright (c) 2014 The New York Times Company. All Rights Reserved.",
+   "results":[
+      {
+         "congress": "113",
+         "bill": "H.R.2397",
+         "url_number": "hr2397",
+         "title": "Department of Defense Appropriations Act, 2014",
+         "sponsor": "C. W. Bill Young",
+         "sponsor_id": "Y000031",
+         "introduced_date": "2013-06-17",
+         "cosponsors": "0",
+         "committees": "House Appropriations; Senate Appropriations",
+         "latest_major_action_date": "2013-07-30",
+         "latest_major_action": "Referred to Senate committee. Status: Received in the Senate and Read twice and referred to the Committee on Appropriations.Note: For further action, see H.R.3547 , which became Public Law 113-76 on 1/17/2014.",
+         "house_passage_vote": "2013-07-24",
+         "senate_passage_vote": "",
+                  "related_bills":[
+]
+      }
+   ]
+}
---
+++ data/bills-recent.js
@@ -0,0 +1,45 @@
+var billsRecent = {
+  "status": "OK",
+  "copyright": "Copyright (c) 2014 The New York Times Company. All Rights Reserved.",
+  "results": [
+   {
+    "congress": "113",
+    "chamber": "House",
+    "num_results": "3",
+    "offset": "0",
+    "bills": [
+      {
+       "number": "H.RES.567",
+       "bill_uri": "http://api.nytimes.com/svc/politics/v3/us/legislative/congress/113/bills/hres567.json",
+       "title": "Providing for the Establishment of the Select Committee on the Events Surrounding the 2012 Terrorist Attack in Benghazi.",
+       "sponsor_uri": "http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/S000250.json",
+       "introduced_date": "May  6, 2014",
+       "cosponsors": "0",
+       "committees": "House Rules",
+       "latest_major_action_date": "May  8, 2014",
+       "latest_major_action": "Mr. Sessions asked unanimous consent to modify H. Res. 567 in the form he had placed at the desk. Without objection, the modification was agreed to.(consideration: CR H3985)"
+      }, {
+       "number": "H.R.2642",
+       "bill_uri": "http://api.nytimes.com/svc/politics/v3/us/legislative/congress/113/bills/hr2642.json",
+       "title": "Agricultural Act of 2014",
+       "sponsor_uri": "http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/L000491.json",
+       "introduced_date": "July 10, 2013",
+       "cosponsors": "0",
+       "committees": "House Agriculture",
+       "latest_major_action_date": "February  4, 2014",
+       "latest_major_action": "Became Public Law No: 113-79"
+      }, {
+       "number": "H.R.325",
+       "bill_uri": "http://api.nytimes.com/svc/politics/v3/us/legislative/congress/113/bills/hr325.json",
+       "title": "To ensure the complete and timely payment of the obligations of the United States Government until May 19, 2013, and for other purposes.",
+       "sponsor_uri": "http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/C000071.json",
+       "introduced_date": "January 21, 2013",
+       "cosponsors": "1",
+       "committees": "House Foreign Affairs",
+       "latest_major_action_date": "February  4, 2013",
+       "latest_major_action": "Became Public Law No: 113-3"
+      }
+    ]
+   }
+  ]
+}
